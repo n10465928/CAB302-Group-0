88a4e02fc95bc68563ab9c4bbadfe37e16ce650c
Avextro <46745919+Avextro@users.noreply.github.com>
2024-09-18 17:35:57 +1000

    login inputs unit test frame
    created framework for testing login input validation methods

---
+++ Group Assignment/src/test/java/loginInputsTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.*;
+import vpm.gui_prototype.controllers.LoginController;
+
+public class loginInputsTest {
+    LoginController loginController;
+
+    @BeforeEach
+    void setup(){
+        loginController = new LoginController();
+    }
+
+    @Test
+    void testAllFieldsFilled(){
+    }
+
+    @Test
+    void testEachFieldEmpty(){
+    }
+}
