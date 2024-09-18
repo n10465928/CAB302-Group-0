64b340e2b146274b458c94672eeb6985eac06e46
Avextro <46745919+Avextro@users.noreply.github.com>
2024-09-18 17:44:45 +1000

    unit tests passed
    valid inputs method created and passed unit tests

--- Group Assignment/src/test/java/loginInputsTest.java
+++ Group Assignment/src/test/java/loginInputsTest.java
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
 import vpm.gui_prototype.controllers.LoginController;
 
 public class loginInputsTest {
@@ -11,9 +12,12 @@ public class loginInputsTest {
 
     @Test
     void testAllFieldsFilled(){
+        assertTrue(loginController.validInputs("user", "pass"));
     }
 
     @Test
     void testEachFieldEmpty(){
+        assertFalse(loginController.validInputs("user", ""));
+        assertFalse(loginController.validInputs("", "pass"));
     }
 }
