393df6c26f85489910e6194b449bcfb22d8f957f
n10465928 <n10465928@qut.edu.au>
2024-10-25 20:31:28 +1000

    Added Food Unit Tests

--- src/test/java/FoodTest.java
+++ src/test/java/FoodTest.java
@@ -1,6 +1,5 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import vpm.gui_prototype.models.FoodStuff.Bone;
 import vpm.gui_prototype.models.FoodStuff.Food;
 
 import static org.junit.jupiter.api.Assertions.*;
